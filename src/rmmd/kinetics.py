bda800ac6c80326ca9559a720aa526eddbb5b688
Florian Solbach <138146987+flrnslbch@users.noreply.github.com>
2026-07-03 19:57:13 +0200

    Add Kinetics Fitting Calculation

--- src/rmmd/kinetics.py
+++ src/rmmd/kinetics.py
@@ -1,8 +1,11 @@
 from typing import Annotated, Literal
 
-from pydantic import Field
+from pydantic import Discriminator, Field, Tag
+from .calc import CalculationBase, CalculationInputBase, CalculationOutputBase
+from .thermo import FittedToLiterature, FittedToOtherCalculation
+from ._base import RmmdBaseModel
 
-from .keys import CitationKey
+from .keys import CitationKey, KineticsIndex
 from .registry import HasKeyMixin
 
 
@@ -34,16 +37,90 @@ class ModifiedArrhenius(_RateCoefficientBase):
 class RateTable(_RateCoefficientBase):
     """Rate coefficient table for a specific reaction"""
 
-    type: Literal["T rate table"] = "T rate table"
+    type: Literal["rate table"] = "rate table"
 
     T: list[float]
     """Temperature points in K"""
-    p: list[float] | float
+    p: list[float]
     """Pressure points in Pa"""
-    k: list[float]
-    """rate coefficients in SI units"""
+    k: list[list[float]]
+    """rate coefficients in SI units
+
+    n x m array of rate constants where n is the number of pressure points and m is the
+    number of temperature points.
+    """
 
 
 # TODO add rates from TST, master equation, etc.
 
 RateCoefficient = Annotated[ModifiedArrhenius | RateTable, Field(discriminator="type")]
+
+
+###############################################################################
+# fitting provenance
+###############################################################################
+
+
+class FittedToKineticData(RmmdBaseModel):
+    """kinetic datat that the coefficients of this model were fitted to"""
+
+    rate_constants: list[KineticsIndex]
+    """kinetic datat that the coefficients of this model were fitted to"""
+
+
+# annotated union for better error messages:
+def _fitted_to_discriminator(value: dict | RmmdBaseModel) -> str | None:
+    if isinstance(value, dict):
+        if "sources" in value:
+            return "literature"
+        elif "rate_constants" in value:
+            return "kinetic data"
+        elif "output_of" in value:
+            return "calculation output"
+
+    else:
+        if hasattr(value, "sources"):
+            return "literature"
+        elif hasattr(value, "rate_constants"):
+            return "kinetic data"
+        elif hasattr(value, "output_of"):
+            return "calculation output"
+
+    # Could not determine the type, return None to let Pydantic handle the error
+    return None
+
+
+_FittedToUnion = Annotated[
+    Annotated[FittedToLiterature, Tag("literature")]
+    | Annotated[FittedToKineticData, Tag("kinetic data")]
+    | Annotated[FittedToOtherCalculation, Tag("calculation output")],
+    Discriminator(_fitted_to_discriminator),
+]
+
+
+class ThermoParameterFittingInput(CalculationInputBase):
+    """input data for a fitting calculation"""
+
+    fitted_to: _FittedToUnion | None = None
+    """data/model that the coefficients of this model were fitted to.
+
+    If the model was fitted to data form this dataset, an integer (starting at
+    0) to indicate the index of the data in the thermo list of this species.
+    """
+
+
+class ThermoParameterFittingOutput(CalculationOutputBase):
+    """output data for a fitting calculation"""
+
+    thermo: KineticsIndex
+    """model parameters that were fitted to the data"""
+
+    # TODO add fit quality metric
+
+
+class ThermoParameterFitting(
+    CalculationBase[ThermoParameterFittingInput, ThermoParameterFittingOutput]
+):
+    """calculation of thermochemical properties derived from fitting to data"""
+
+    type: Literal["thermo param fit"] = "thermo param fit"
